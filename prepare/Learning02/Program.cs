619eb3c8ae1525e31b0d3a501d3dff31212fb614
TravelCurry02 <aceguy02@gmail.com>
2024-11-12 20:36:57 -0500

    Done with prepare number 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job01 = new Job();
+        job01._jobTitle = "Software Engineer";
+        job01._companyName = "Microsoft";
+        job01._startYear = 2012;
+        job01._endYear = 2020;
+
+        Job job02 = new Job();
+        job02._jobTitle = "Supervisor";
+        job02._companyName = "Apple";
+        job02._startYear = 2020;
+        job02._endYear = 2024; 
+
+        Resume my_Resume = new Resume();
+        my_Resume._name = "Iguss Self";
+
+        my_Resume._jobs.Add(job01);
+        my_Resume._jobs.Add(job02);
+
+        my_Resume.Display();
+
+
     }
 }
